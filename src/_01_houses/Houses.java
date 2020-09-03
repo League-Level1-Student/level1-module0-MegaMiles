f05813ae110e8660e72207a759ef3ba7597fecb1
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-09-02 18:20:17 -0700

    Houses.java

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,6 +1,7 @@
 package _01_houses;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
@@ -10,41 +11,66 @@ public class Houses {
 	sob.setSpeed(100);
 	sob.setY(450);
 	sob.setX(50);
-	sob.penDown();	
+	sob.penDown();
+	Random lol = new Random();
 for (int i = 0; i < 10; i++) {
-	builder();
-}	
+	int yay = lol.nextInt(3);
+	if (yay==0) {
+		drawpointyroof("small",Color.BLUE);	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	if (yay==1) {
+		drawpointyroof("medium",Color.RED);	
 	}
-	
-	 void builder() {
-	sob.move(300);	
-	sob.turn(90);	
-	sob.move(50);	
-	sob.turn(90);	
-	sob.move(300);
+	if (yay==2) {
+		drawFlatroof("large",Color.GREEN);	
+	}
+}
+	}
+	int househeight = 100;
+	 void drawpointyroof(String height, Color c) {
+	if (height.equals("small")) {
+	 househeight = 60;	
+	}
+	if (height.equals("medium")) {
+		 househeight = 120;	
+		}
+	if (height.equals("large")) {
+		 househeight = 250;	
+		}
+	sob.setRandomPenColor();
+	sob.move(househeight);	
+	sob.turn(45);	
+	sob.move(30);	
+	sob.turn(90);
+	sob.move(30);
+	sob.turn(45);
+	sob.move(househeight);
 	sob.turn(-90);
 	sob.setPenColor(Color.green);
 	sob.move(30);
 	sob.setPenColor(Color.black);
 	sob.turn(-90);
 	 }
-
-
-
-
-
+	 void drawFlatroof(String height, Color c) {
+			if (height.equals("small")) {
+			 househeight = 60;	
+			}
+			if (height.equals("medium")) {
+				 househeight = 120;	
+				}
+			if (height.equals("large")) {
+				 househeight = 250;	
+				}
+			sob.setRandomPenColor();
+			sob.move(househeight);	
+			sob.turn(90);	
+			sob.move(50);	
+			sob.turn(90);
+			sob.move(househeight);
+			sob.turn(-90);
+			sob.setPenColor(Color.green);
+			sob.move(30);
+			sob.setPenColor(Color.black);
+			sob.turn(-90);
+			 }
 }
