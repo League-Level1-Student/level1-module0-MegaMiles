3973b2b4a8ef6d3fbddef90889c7a1f4e20a9c22
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-09-09 18:23:51 -0700

    PhotoQuiz.Java

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,27 +26,31 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+String yay = "https://www.sideshow.com/storage/product-images/904599/iron-man-mark-lxxxv__silo.png";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component yayay;
 		// 3. use the "createImage()" method below to initialize your Component
-
+yayay = createImage(yay);
 		// 4. add the image to the quiz window
-
+quizWindow.add(yayay);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String yayyayyayyay = JOptionPane.showInputDialog("Is this Mark 63?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if (yayyayyayyay.equalsIgnoreCase("No")) {
+JOptionPane.showMessageDialog(null,"CORRECT CONGRADULATIONS!!!!!!");	
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null,"INCORECT Go study more iron man");	
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+        
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
