a8607ed3d3329a7990221866596f393fce3e2e94
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-09-16 18:26:32 -0700

    bodyPartQuizRunner.java

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -47,19 +47,27 @@ else {
 }
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(yayay);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-        
+String Hi = "https://www.sideshow.com/storage/product-images/903421/iron-man_marvel_silo.png";
 		// 11. add the second image to the quiz window
+Component bob;
+bob = createImage(Hi);
 
+quizWindow.add(bob);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String Jeff = JOptionPane.showInputDialog("Is this suit of armour nicknamed the bleeding edge armour?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
-	}
+if (Jeff.equalsIgnoreCase("Yes")) {
+JOptionPane.showMessageDialog(null,"You got it correct CONGRADULATIONS!!!!!!!!!!!!");
+}
+else {
+	JOptionPane.showMessageDialog(null,"You got it wrong booooo go study more iron man boooooooooooo");	
+}
+}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
