acf83301a17b6084db2a96939c479f435a4489e7
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-09-30 18:26:34 -0700

    jackInTheBox.java

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,9 +1,14 @@
 package _07_fortune_cookie;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class FortuneCookie {
+public class FortuneCookie implements ActionListener {
 
 	public void showButton() {
 		// TODO Auto-generated method stub
@@ -12,6 +17,8 @@ public class FortuneCookie {
 		JButton button = new JButton();
 	    frame.add(button);
 	    frame.pack();
+	    button.addActionListener(this);
+	   
 	
 	
 	
@@ -22,8 +29,30 @@ public class FortuneCookie {
 	
 	
 	
-	
-	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Woohoo");
+		 int rand = new Random().nextInt(5);
+		 if (rand == 0) {
+			    JOptionPane.showMessageDialog(null, "hi");
+		 }
+		 if (rand == 1) {
+				JOptionPane.showMessageDialog(null, "yo");
+		 }
+		 if (rand == 2) {
+				JOptionPane.showMessageDialog(null, "wasup");
+		 }
+		 if (rand == 3) {
+				JOptionPane.showMessageDialog(null, "sup");
+		 }
+		 if (rand == 4) {
+				JOptionPane.showMessageDialog(null, "hello");
+		 }
+		 
+		 
 	}
 
 }
---
+++ src/_08_jack_in_the_box/JackInTheBox.java
@@ -0,0 +1,10 @@
+package _08_jack_in_the_box;
+
+import javax.swing.JOptionPane;
+
+public class JackInTheBox {
+public static void main(String[] args) {
+
+
+}
+}
