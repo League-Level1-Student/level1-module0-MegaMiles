1c05faa4deccba206248f8aa7e3619863d426cea
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2020-10-14 18:32:05 -0700

    TV.java

--- src/_09_sound_effects_machine/Soundeffects.java
+++ src/_09_sound_effects_machine/Soundeffects.java
@@ -1,47 +1,71 @@
 package _09_sound_effects_machine;
 
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Soundeffects implements ActionListener {
+	JButton button = new JButton();
+	JButton jeff = new JButton();
+	JButton hi = new JButton();
+	JButton Koolboi = new JButton();
 	public static void main(String[] args) {
 		Soundeffects hi = new Soundeffects();
 		hi.HIIII();
 	}
 
+	public void YOYo() {
+		for (int i = 0; i < 1000000000; i++) {
+			playSound("pizza.wav");
+		}	
+	}
+	
 	public void HIIII() {
 		JPanel panel = new JPanel();
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		
-		JButton button = new JButton();
-		panel.add(button);
 		frame.add(panel);
 		button.addActionListener(this);
+		panel.add(button);
+		button.setText("100");
 		
-		JButton jeff = new JButton();
 		panel.add(jeff);
 		jeff.addActionListener(this);
+		jeff.setText("200");
 		
-		JButton hi = new JButton();
 		panel.add(hi);
 		hi.addActionListener(this);
+		hi.setText("300");
 		
-		JButton yo = new JButton();
-		panel.add(yo); 
-		yo.addActionListener(this);
+		panel.add(Koolboi);
+		Koolboi.addActionListener(this);
+		Koolboi.setText("PIZZA");
 		frame.pack();
-
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+	if (button == e.getSource()) {
+		playSound("hi.mp3");
 	}
-
+	else if (jeff == e.getSource()) {
+		playSound("pizza.wav");
+	}
+	else if (hi == e.getSource()) {
+		playSound("yay.wav");
+	}
+	else if (Koolboi == e.getSource()) {
+		YOYo();
+	}
+	}
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	     sound.play();
+	}	
 }
---
+++ src/_10_cuteness_tv/TV.java
@@ -0,0 +1,249 @@
+package _10_cuteness_tv;
+
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TV {
+public static void main(String[] args) {
+
+	
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	
+JButton yoyo = new JButton();	
+JButton yeeet = new JButton();	
+JButton bruh = new JButton();	
+	
+frame.add(panel);
+panel.add(yoyo);
+yoyo.setText("Frog");
+
+frame.add(panel);
+panel.add(yeeet);
+yeeet.setText("Duck");
+
+frame.add(panel);
+panel.add(bruh);
+bruh.setText("Unicorns");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't 
+	     //work on some Linux implementations 
+	     try {
+	     if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	     if (Runtime.getRuntime().exec(new String[] { 
+	      "which", "xdg- open" }).getInputStream().read() != -1) {
+	     Runtime.getRuntime().exec(new String[] { "xdg-open", videoID }); 
+	     }
+	     } else {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     }      } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
